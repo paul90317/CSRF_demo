e392c72470b73ad7d4aa9fc49347c7bd7c42c807
paul90317 <paul2309825@gmail.com>
2020-09-28 17:27:58 +0800

    nnn

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var yourname = '';
 var io = require("socket.io")(http)
 io.on("connection",function(socket){
 	socket.on("login",function(name){
-		console.log(name+" connect ...");
+		console.log(name+" login ...");
 		yourname = name;
 	})
 	socket.on("transfer",function(towho,money){
-		if(yourname=="you")console.log(yourname+" transfer "+money+" to "+towho);
+		if(yourname=="paul90317")console.log(yourname+" transfer "+money+" to "+towho);
 		else console.log("transfer fail!");
 	})
 })
